20b36b9971c1a58076c92ff6164b4451c79f7c20
marcelpinotti <marcel.pinotti@gmail.com>
2021-09-07 00:23:51 -0300

    Projeto Criado

---
+++ backend/src/main/java/com/marcelpinotti/sdvendas/SdvendasApplication.java
@@ -0,0 +1,13 @@
+package com.marcelpinotti.sdvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdvendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/marcelpinotti/sdvendas/SdvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcelpinotti.sdvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
