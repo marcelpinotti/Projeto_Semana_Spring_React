f9c6d239532d354bafaf566baaef92a3513bb2c9
marcelpinotti <marcel.pinotti@gmail.com>
2021-09-11 19:42:29 -0300

    Primeira solicitacao - request

--- frontend/src/components/DonutChart/dist/index.js
+++ frontend/src/components/DonutChart/dist/index.js
@@ -11,17 +11,30 @@ var __assign = (this && this.__assign) || function () {
     return __assign.apply(this, arguments);
 };
 exports.__esModule = true;
+var axios_1 = require("axios");
 var react_apexcharts_1 = require("react-apexcharts");
 var DonutChart = function () {
-    var mockData = {
-        series: [477138, 499928, 444867, 220426, 473088],
-        labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    };
+    //forma errada mas roda (o correto é chamar rotas, etc)
+    var chartData = { labels: [], series: [] };
+    //requisição back-end
+    //forma errada 
+    axios_1["default"].get('${BASE_URL}/sales/amount-by-seller')
+        .then(function (response) {
+        var data = response.data;
+        var myLabels = data.map(function (x) { return x.sellerName; });
+        var mySeries = data.map(function (x) { return x.sum; });
+        chartData = { labels: myLabels, series: mySeries };
+        console.log(chartData);
+    });
+    //const mockData = {
+    //    series: [477138, 499928, 444867, 220426, 473088],
+    //    labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
+    //}
     var options = {
         legend: {
             show: true
         }
     };
-    return (React.createElement(react_apexcharts_1["default"], { options: __assign(__assign({}, options), { labels: mockData.labels }), series: mockData.series, type: "donut", height: "240" }));
+    return (React.createElement(react_apexcharts_1["default"], { options: __assign(__assign({}, options), { labels: chartData.labels }), series: chartData.series, type: "donut", height: "240" }));
 };
 exports["default"] = DonutChart;
---
+++ frontend/src/utils/dist/requests.js
@@ -0,0 +1,4 @@
+"use strict";
+exports.__esModule = true;
+exports.BASE_URL = void 0;
+exports.BASE_URL = "http://localhost:8080";
