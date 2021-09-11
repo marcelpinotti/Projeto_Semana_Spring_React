f2161e56b412693dad79d9dd12ef594fd4e1e580
marcelpinotti <marcel.pinotti@gmail.com>
2021-09-10 23:05:02 -0300

    Home e navegação

--- frontend/src/components/NavBar/dist/index.js
+++ frontend/src/components/NavBar/dist/index.js
@@ -1,10 +1,12 @@
 "use strict";
 exports.__esModule = true;
 var ds_dark_svg_1 = require("assets/img/ds-dark.svg");
+var react_router_dom_1 = require("react-router-dom");
 var NavBar = function () {
     return (React.createElement("div", { className: "d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm" },
         React.createElement("div", { className: "container" },
             React.createElement("nav", { className: "my-2 my-md-0 mr-md-3" },
-                React.createElement("img", { src: ds_dark_svg_1["default"], alt: "DevSuperior", width: "120" })))));
+                React.createElement(react_router_dom_1.Link, { to: "/" },
+                    React.createElement("img", { src: ds_dark_svg_1["default"], alt: "DevSuperior", width: "120" }))))));
 };
 exports["default"] = NavBar;
--- frontend/src/pages/Home/dist/index.js
+++ frontend/src/pages/Home/dist/index.js
@@ -1,7 +1,18 @@
 "use strict";
 exports.__esModule = true;
+var Footer_1 = require("components/Footer");
+var NavBar_1 = require("components/NavBar");
+var react_router_dom_1 = require("react-router-dom");
 var Home = function () {
-    return (React.createElement("div", null,
-        React.createElement("h1", null, "HOME PAGE")));
+    return (React.createElement(React.Fragment, null,
+        React.createElement(NavBar_1["default"], null),
+        React.createElement("div", { className: "container" },
+            React.createElement("div", { className: "jumbotron" },
+                React.createElement("h1", { className: "display-4" }, "DSVendas"),
+                React.createElement("p", { className: "lead" }, "Analise o desempenho das suas vendas por diferentes perspectivas"),
+                React.createElement("hr", null),
+                React.createElement("p", null, "Esta aplica\u00E7\u00E3o consiste em exibir um dashboard a partir de dados fornecidos por um back end constru\u00EDdo com Spring Boot."),
+                React.createElement(react_router_dom_1.Link, { className: "btn btn-primary btn-lg", to: "/dashboard" }, "Acessar dashboard"))),
+        React.createElement(Footer_1["default"], null)));
 };
 exports["default"] = Home;
