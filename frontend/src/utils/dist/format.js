820d448f0ce1a87b90461ef82a9d93883903b569
marcelpinotti <marcel.pinotti@gmail.com>
2021-09-11 23:06:58 -0300

    Integração da DataTable

--- frontend/src/utils/dist/format.js
+++ frontend/src/utils/dist/format.js
@@ -1,7 +1,13 @@
 "use strict";
 exports.__esModule = true;
-exports.round = void 0;
+exports.formatLocalDate = exports.round = void 0;
+var date_fns_1 = require("date-fns");
 exports.round = function (value, precision) {
     var multiplier = Math.pow(10, precision || 0);
     return Math.round(value * multiplier) / multiplier;
+};
+exports.formatLocalDate = function (date, pattern) {
+    var dt = new Date(date);
+    var dtDateOnly = new Date(dt.valueOf() + dt.getTimezoneOffset() * 60 * 1000);
+    return date_fns_1.format(dtDateOnly, pattern);
 };
