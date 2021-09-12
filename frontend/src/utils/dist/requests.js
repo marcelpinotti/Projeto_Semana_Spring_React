cb8e9ac98673e0606fdf406b5486f4cdddedae80
marcelpinotti <marcel.pinotti@gmail.com>
2021-09-12 00:17:20 -0300

    Variavel de Ambiente

--- frontend/src/utils/dist/requests.js
+++ frontend/src/utils/dist/requests.js
@@ -1,4 +1,5 @@
 "use strict";
+var _a;
 exports.__esModule = true;
 exports.BASE_URL = void 0;
-exports.BASE_URL = 'http://localhost:8080';
+exports.BASE_URL = (_a = process.env.REACT_APP_BACKEND_URL) !== null && _a !== void 0 ? _a : 'http://localhost:8080';
